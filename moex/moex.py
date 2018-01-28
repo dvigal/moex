3c7ae5200bcc5e9d5541673c25ee232e1828b80b
dvigal <alvilitvinov@nsk.beeline.ru>
2018-01-28 22:50:53 +0700

    Code refactoring

--- moex/moex.py
+++ moex/moex.py
@@ -11,58 +11,62 @@ import pandas as pd
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 
-class moex(object):
+def _parse_data_node(node):
+    columns = list()
+    data = list()
+    for child in node.getchildren():
+        if child.tag == "metadata":
+            metadata_children = child.getchildren()
+            for metadata_child in metadata_children:
+                if metadata_child.tag == "columns":
+                    for columns_child in metadata_child.getchildren():
+                        if columns_child.attrib and columns_child.attrib.get("name") is not None:
+                            columns.append(columns_child.attrib.get("name"))
+        if child.tag == "rows":
+            rows = child.getchildren()
+            for row in rows:
+                row_for_data = list()
+                data.append(row_for_data)
+                for column in columns:
+                    if column != 'DATE':
+                        row_for_data.append(row.attrib.get(column))
+
+    return pd.DataFrame(data, columns=columns)
+
+
+def _xml_to_df(xml, *target_child_id):
+    '''
+    :return: Return list of data frames
+    '''
+    frames = list()
+    for node in xml.getchildren():
+        if node.tag == 'data' and node.attrib and node.attrib.get("id") in target_child_id[0]:
+            frames.append(_parse_data_node(node))
+
+    return frames
+
+
+def _sanitize(dataframe):
+    for column in dataframe.columns:
+        dataframe[column] = dataframe[column].apply(lambda value: None if value == '' else value)
+
+    return dataframe
+
+
+def _load_url(url):
+    logging.debug("url='%s'", url)
+    text = request.urlopen(url).read()
+
+    return text
+
+
+class MOEX(object):
     """
     ISS MOEX API: http://iss.moex.com/iss/reference/
     """
     def __init__(self, processes=None) -> None:
         super().__init__()
         self.__pool = Pool(processes=processes)
-
-    def _load_url(self, url):
-        logging.debug("url='%s'", url)
-        text = request.urlopen(url).read()
-
-        return text
-
-    def _sanitize(self, dataframe):
-        for column in dataframe.columns:
-            dataframe[column] = dataframe[column].apply(lambda value: None if value == '' else value)
-
-        return dataframe
-
-    def _parse_data_node(self, node):
-        columns = list()
-        data = list()
-        for child in node.getchildren():
-            if child.tag == "metadata":
-                metadata_children = child.getchildren()
-                for metadata_child in metadata_children:
-                    if metadata_child.tag == "columns":
-                        for columns_child in metadata_child.getchildren():
-                            if columns_child.attrib and columns_child.attrib.get("name") is not None:
-                                columns.append(columns_child.attrib.get("name"))
-            if child.tag == "rows":
-                rows = child.getchildren()
-                for row in rows:
-                    row_for_data = list()
-                    data.append(row_for_data)
-                    for column in columns:
-                        if column != 'DATE':
-                            row_for_data.append(row.attrib.get(column))
-
-        return pd.DataFrame(data, columns=columns)
-
-    def _xml_to_df(self, xml, *target_child_id):
-        '''
-        :return: Return list of data frames
-        '''
-        frames = list()
-        for node in xml.getchildren():
-            if node.tag == 'data' and node.attrib and node.attrib.get("id") in target_child_id:
-                frames.append(self._parse_data_node(node))
-
-        return frames
 
     def securities(self, q=None, limit=100, lang='ru'):
         """
@@ -71,7 +75,17 @@ class moex(object):
         """
         base_url = "https://iss.moex.com/iss/securities.xml"
         base_url = "{base_url}?q={query}".format(base_url=base_url, query=q) if q is not None else base_url
-        return self._xml_to_df(etree.fromstring(self._load_url(base_url)), "securities")[0]
+        return _xml_to_df(etree.fromstring(_load_url(base_url)), "securities")[0]
+
+    def securities_aggregates(self, security):
+        '''
+        URL: https://iss.moex.com/iss/reference/214\n
+        :return: Return Pandas DataFrame
+        '''
+
+        base_url = "https://iss.moex.com/iss/securities/{security}/aggregates.xml"
+        return _xml_to_df(etree.fromstring(_load_url(base_url.format(security=security))), "securities")[0]
+
 
     def securities_indicies(self, security, only_actual=0, lang='ru'):
         """
@@ -80,7 +94,18 @@ class moex(object):
         """
         base_url = "https://iss.moex.com/iss/securities/{security}/indices.xml?only_actual={only_actual}&lang={lang}".format(
             security=security, only_actual=only_actual, lang=lang)
-        return self._xml_to_df(etree.fromstring(self._load_url(base_url)), "indices")[0]
+        return _xml_to_df(etree.fromstring(_load_url(base_url)), "indices")[0]
+
+    " https://iss.moex.com/iss/turnovers.xml"
+
+    def turnovers(self):
+        """
+        URL: https://iss.moex.com/iss/reference/24\n
+        :return: Pandas DataFrame
+        """
+        base_url = "https://iss.moex.com/iss/turnovers.xml"
+        children = ["turnovers", "turnoversprevdate", "turnoverssectors", "turnoverssectorsprevdate"]
+        return _xml_to_df(etree.fromstring(_load_url(base_url)), children)
 
     def enginies(self):
         """
@@ -88,7 +113,7 @@ class moex(object):
         :return: Pandas DataFrame
         """
         base_url = "https://iss.moex.com/iss/engines.xml"
-        return self._xml_to_df(etree.fromstring(self._load_url(base_url)), "engines")[0]
+        return _xml_to_df(etree.fromstring(_load_url(base_url)), "engines")[0]
 
     def statistics_engines_futures_markets_indicativerates_securities(self, date_start=None, date_end=None):
         """
@@ -117,8 +142,8 @@ class moex(object):
 
         urls = [mk_url((date_start + timedelta(days=day)).strftime("%Y-%m-%d")) for day in range(0, days)]
 
-        for document in self.__pool.map(self._load_url, urls):
-            df = self._xml_to_df(etree.fromstring(document), "securities")[0]
+        for document in self.__pool.map(_load_url, urls):
+            df = _xml_to_df(etree.fromstring(document), "securities")[0]
             df = df[df["BOARDID"] == "MRKT"]
             df = df[df["SECID"].isin(secid)]
             df = df[columns]
@@ -137,7 +162,7 @@ class moex(object):
         if indexid is None:
             base_url = "https://iss.moex.com//iss/statistics/engines/stock/markets/index/analytics.xml?lang={lang}".format(
                 lang=lang)
-            return self._xml_to_df(etree.fromstring(self._load_url(base_url)), "indices")[0]
+            return _xml_to_df(etree.fromstring(_load_url(base_url)), "indices")[0]
         else:
             base_url = "https://iss.moex.com//iss/statistics/engines/stock/markets/index/analytics/{indexid}.xml"
             parameters = \
@@ -146,7 +171,7 @@ class moex(object):
                 "start={start}&" \
                 "date={date}".format(lang=lang, limit=limit, start=start, date=date)
             base_url = base_url + "?" + parameters
-            return self._xml_to_df(etree.fromstring(self._load_url(base_url)), "indices")[0]
+            return _xml_to_df(etree.fromstring(_load_url(base_url)), "indices")[0]
 
     def statistics_engines_stock_capitalization(self, date_start=None, date_end=None):
         """
@@ -169,8 +194,8 @@ class moex(object):
 
         urls = [mk_url((date_start + timedelta(days=day)).strftime("%Y-%m-%d")) for day in range(0, days)]
 
-        for document in self.__pool.map(self._load_url, urls):
-            frames = self._xml_to_df(etree.fromstring(document), "capitalization")
+        for document in self.__pool.map(_load_url, urls):
+            frames = _xml_to_df(etree.fromstring(document), "capitalization")
             result = pd.concat([result, frames[0] if len(frames) > 0 else pd.DataFrame([])])
 
         result.reset_index(drop=True, inplace=True)
@@ -179,13 +204,14 @@ class moex(object):
         result.dropna(inplace=True)
         return result
 
-    def history_engines_markets_securities(self, engine, market, boardgroup, *securities, date_start=None, date_end=None, limit=100, start=0):
+    def history_engines_markets_securities(self, engine, market, boardgroup, *securities, date_start=None, date_end=None, limit=100, start=0, security_collection=None):
         '''
         URL: https://iss.moex.com/iss/reference/29\n
         :return: Pandas DataFrame
         '''
         base_url = "https://iss.moex.com/iss/history/engines/{engine}/markets/{market}/boardgroups/{boardgroup}/securities.xml".format(engine=engine, market=market, boardgroup=boardgroup)
         base_url = base_url + "?limit={limit}&start={start}&date={date}&securities={securities}"
+        base_url = base_url + "&security_collection=" + str(security_collection) if security_collection is not None else base_url
 
         result = pd.DataFrame([])
 
@@ -199,13 +225,13 @@ class moex(object):
         securities = str(securities).replace('(', '').replace(')', '').replace("'", '').replace(' ', '')
         urls = [base_url.format(limit=limit, start=start, date=date, securities=securities) for date in dates]
 
-        for document in self.__pool.map(self._load_url, urls):
-            frames = self._xml_to_df(etree.fromstring(document), "history")
+        for document in self.__pool.map(_load_url, urls):
+            frames = _xml_to_df(etree.fromstring(document), "history")
             result = pd.concat([result, frames[0] if len(frames) > 0 else pd.DataFrame([])])
 
         result.reset_index(drop=True, inplace=True)
 
-        self._sanitize(result)
+        _sanitize(result)
 
         # result.dropna(inplace=True)
 
@@ -218,4 +244,4 @@ class moex(object):
         '''
         base_url = "https://iss.moex.com/iss/engines/{engine}/markets/{market}/boardgroups.xml".format(engine=engine, market=market)
 
-        return self._xml_to_df(etree.fromstring(self._load_url(base_url)), "boardgroups")[0]
+        return _xml_to_df(etree.fromstring(_load_url(base_url)), "boardgroups")[0]
